450c8fc49f05c33a5e960284d548c2af64dbfd11
CoolJesse <jesstolson@gmail.com>
2018-11-07 00:54:26 -0800

    second lesson from Udemity Java course.

---
+++ tutorial_2/src/tutorial_2/application.java
@@ -0,0 +1,31 @@
+package tutorial_2;
+
+public class application {
+
+	public static void main(String[] args) {
+		int myNumber_1;
+		int myNumber_2 = 69;
+		
+		short myShort = 11;
+		long myLong = 100000;
+		double myDouble = 3.14;
+		float myFloat = 69.69f; //add f at end to make float
+		char myChar = 'A';
+		boolean myBoolean = true;
+		byte myByte = 127;
+		
+		
+		myNumber_1 = 88;
+		
+		System.out.println(myNumber_1);
+		System.out.println("The value of myNumber_2 is: " + myNumber_2);
+		System.out.println("The value of myShort is: " + myShort);
+		System.out.println("The value of myLong is: " + myLong);
+		System.out.println("The value of myDouble is: " + myDouble);
+		System.out.println("The value of myFloat is: " + myFloat);
+		System.out.println("The value of myChar is: " + myChar);
+		System.out.println("The value of myBoolean is: " + myBoolean);
+		System.out.println("The value of myByte is: " + myByte);
+	}
+
+}
