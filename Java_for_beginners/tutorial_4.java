598957e33b066bf19362900aef95a89d73a16cc9
CoolJesse <jesstolson@gmail.com>
2018-11-28 00:02:20 -0800

    finished tutorial 5 and 6 from Java for Complete Beginners. Started Java Master Class.

---
+++ Java_for_beginners/tutorial_4.java
@@ -0,0 +1,17 @@
+/*while loops*/
+
+public class tutorial_4{
+	
+	public static void main(String[] args){
+		
+		int value = 0;
+		
+		while(value < 10){
+			System.out.println(value);
+			value += 1;
+		}
+		
+		System.out.println(value);
+	}
+
+}
