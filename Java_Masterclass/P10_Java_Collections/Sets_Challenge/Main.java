a1443304bf277552a3162bcd38f5b5772a4cb9da
CoolJesse <jesstolson@gmail.com>
2019-05-24 18:57:59 -0700

    Final version of Set Challenge

--- Java_Masterclass/P10_Java_Collections/Sets_Challenge/Main.java
+++ Java_Masterclass/P10_Java_Collections/Sets_Challenge/Main.java
@@ -6,10 +6,48 @@ import java.util.LinkedHashMap;
 import java.util.Iterator;
 
 public class Main{
+/** Helper class to form key of solar system map *************************************/
+	public static class Pair{
+		private String name;
+		private String type;
+		
+		public Pair(String name, String type){
+			this.name = name;
+			this.type = type;
+		}
+		public Pair(){
+			this("","");
+		}
+		
+		@Override
+		public String toString(){
+			return this.name + " " + this.type;
+		}
+		
+		@Override
+		public boolean equals(Object obj){
+			if(this == obj)
+				return true;
+			
+			if( (obj == null) || (this.getClass() != obj.getClass()) )
+				return false;
+			
+			String objName = ((Pair) obj).name;
+			String objType = ((Pair) obj).type;
+			
+			return ( this.name.equals(objName) && this.type.equals(objType) );
+		}
+		
+		@Override
+		public int hashCode(){
+			return ( this.name.hashCode() + this.type.hashCode() );
+		}
+	}
+/** Main body of program *************************************************************/
 	public static void main(String[] args){
 		
 		Set<HeavenlyBody> SolarSystem = new HashSet<>();
-		//Map<Pair<String, String>, HeavenlyBody> SolarMap = new LinkedHashMap<>();
+		Map<Pair, HeavenlyBody> SolarMap = new LinkedHashMap<>();
 	/** Create Stars/the Sun *************************************************/	
 		HeavenlyBody Sun = new Star("The Sun", 0.0);
 	
@@ -55,6 +93,25 @@ public class Main{
 		while(itr.hasNext()){
 			System.out.println(itr.next().printAllInfo());
 		}
+	/** Add bodies to solar system Map *********************************************/
+		SolarMap.put(new Pair(Sun.getName(), Sun.getBodyType()), Sun);
+		SolarMap.put(new Pair(Venus.getName(), Venus.getBodyType()), Venus);
+		SolarMap.put(new Pair(Mercury.getName(), Mercury.getBodyType()), Mercury);
+		SolarMap.put(new Pair(Earth.getName(), Earth.getBodyType()), Earth);
+		SolarMap.put(new Pair(Mars.getName(), Mars.getBodyType()), Mars);
+		SolarMap.put(new Pair(Jupiter.getName(), Jupiter.getBodyType()), Jupiter);
+		SolarMap.put(new Pair(Saturn.getName(), Saturn.getBodyType()), Saturn);
+		SolarMap.put(new Pair(Uranus.getName(), Uranus.getBodyType()), Uranus);
+		SolarMap.put(new Pair(Neptune.getName(), Neptune.getBodyType()), Neptune);
+		
+		for( Map.Entry<Pair, HeavenlyBody> entry : SolarMap.entrySet()){
+			//System.out.println(entry.getValue().printAllInfo());
+			System.out.println("SolarMap " + entry.getValue().toString());
+		}
+		
+		for( Map.Entry<Pair, HeavenlyBody> entry : SolarMap.entrySet()){
+			System.out.println("SolarMap " + entry.getValue().toString());
+		}		
 		
 	}
 }
