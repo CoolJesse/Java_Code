ac6b575c6442dd6f3bb827360bc5c23aadb4113e
CoolJesse <jesstolson@gmail.com>
2019-06-04 01:13:47 -0700

    Learning about Java modules. Intro lesson.

--- Java_Masterclass/P11_Java_Fx/HelloFX.java
+++ Java_Masterclass/P11_Java_Fx/HelloFX.java
@@ -1,27 +1,29 @@
+
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-
 /*
 To compile from command line: $ javac --module-path $JAVA_FX --add-modules javafx.controls HelloFX.java
 
 To run from command line: $ java --module-path $JAVA_FX --add-modules javafx.controls HelloFX
 */
-
 public class HelloFX extends Application {
 
     @Override
     public void start(Stage stage) {
         String javaVersion = System.getProperty("java.version");
         String javafxVersion = System.getProperty("javafx.version");
+		
         Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+		
         Scene scene = new Scene(new StackPane(l), 640, 480);
+		
         stage.setScene(scene);
         stage.show();
     }
-
+/** Main body of program ************************************************************/
     public static void main(String[] args) {
         launch();
     }
